d7bbd96229b09bd17c03f30d1f65d4e081e2c731
shahil shaik <31159193+Shahil-shaik@users.noreply.github.com>
2019-11-23 18:58:45 +0530

    Add files via upload
    final commit

---
+++ 1.b.py
@@ -0,0 +1,39 @@
+import cv2
+import os
+
+face_detector = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+face_id = input('\n enter user id end press <return> ==>  ')
+
+a=input('Enter the path : ')
+cam = cv2.VideoCapture(a)
+
+if (cam.isOpened()== False): 
+  print("Error opening video stream or file")
+
+print("\n [INFO] Initializing face capture. Look the camera and wait ...")
+count = 0
+
+while(cam.isOpened()) :
+    ret, img = cam.read()
+    if ret == True:
+        gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
+        faces = face_detector.detectMultiScale(gray, 1.3, 5)
+
+        for(x,y,w,h) in faces:
+
+            cv2.rectangle(img, (x,y), (x+w,y+h), (255,0,0), 2)     
+            count += 1
+
+            cv2.imwrite("dataset/User." + str(face_id) + '.' + str(count) + ".jpg", gray[y:y+h,x:x+w])
+
+            cv2.imshow('image', img)
+
+        k = cv2.waitKey(10) & 0xff 
+        if k == 27:
+            break
+    else:
+        print("\n [INFO] Exiting Program and cleanup stuff")
+        cam.release()
+        cv2.destroyAllWindows() 
---
+++ 3.a.py
@@ -0,0 +1,40 @@
+import cv2
+import numpy as np
+import os 
+
+recognizer = cv2.face.LBPHFaceRecognizer_create()
+recognizer.read('trainer/trainer.yml')
+faceCascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+
+font = cv2.FONT_HERSHEY_SIMPLEX
+
+i=input('Enter the path : ')
+
+img=cv2.imread(i)
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) 
+faces = faceCascade.detectMultiScale( 
+        gray,
+        scaleFactor = 1.2,
+        minNeighbors = 5
+       ) 
+for(x,y,w,h) in faces:
+
+    cv2.rectangle(img, (x,y), (x+w,y+h), (0,255,0), 2)
+    id, confidence = recognizer.predict(gray[y:y+h,x:x+w])
+    if (confidence < 50):
+        confidence = "  {0}%".format(round(100 - confidence))
+    else:
+        id = "unknown"
+        confidence = "  {0}%".format(round(100 - confidence))
+            
+    cv2.putText(img, str(id), (x+5,y-5), font, 1, (255,255,255), 2)
+    cv2.putText(img, str(confidence), (x+5,y+h-5), font, 1, (255,255,0), 1)  
+    print(id)
+    cv2.imshow('camera',img) 
+
+    k = cv2.waitKey(0) & 0xff 
+    if k == 27:
+        break
+
+print("\n [INFO] Exiting Program and cleanup stuff")
+cv2.destroyAllWindows()
